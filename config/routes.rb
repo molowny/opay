3f070e6e7ffdc1d4dd499e4560e67513e9a40497
Mariusz Ołownia <ollownia@gmail.com>
2013-04-24 12:09:47 +0200

    routes for payu

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Opay::Engine.routes.draw do
 
   # payu
-  # scope :payu do
-  #   post '/online' => 'payu#online', as: online
-  # end
-  post '/online' => 'payu#online', as: online
+  scope 'payu' do
+    post '/online' => 'payu#online', as: :payu_online
+  end
+
 end
